80996520ef28b17d2c20defaf95d6247c8b218d1
Anthony Onwuli <anthony.onwuli16@imperial.ac.uk>
2024-12-11 17:28:04 +0000

    Run pre-commit run --all-files

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-from setuptools import setup, Extension
+# -*- coding: utf-8 -*-
 import numpy as np
 from Cython.Build import cythonize
+from setuptools import Extension, setup
 
 extensions = [
     Extension(
         "mattersim.datasets.utils.threebody_indices",
         ["src/mattersim/datasets/utils/threebody_indices.pyx"],
-        include_dirs=[np.get_include()]
+        include_dirs=[np.get_include()],
     )
 ]
 
-setup(
-    ext_modules=cythonize(extensions)
-)
+setup(ext_modules=cythonize(extensions))
